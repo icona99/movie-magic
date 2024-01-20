580115687b12bf066fd338328fb4af4f92bc6a41
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-01-20 09:32:20 +0200

    Add about page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const handlebars=require('express-handlebars');
 const path=require('path')
+const routes=require('./routes');
 
 
 const app=express();
@@ -12,11 +13,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'views'))
 app.use(express.static( path.join(__dirname,'public')));
+app.use(routes);
 
 
-app.get('/' , (req,res)=>{
-    res.render('home',{layout:false})
-});
+
 
 
 
