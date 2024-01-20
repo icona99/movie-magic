972783a47b1a93083100cc4048823f8c55707a65
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-01-20 10:45:33 +0200

    Fix path issue

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
-const express=require('express');
-const handlebars=require('express-handlebars');
-const path=require('path')
-const routes=require('./routes');
+const express = require('express');
+const routes = require('./routes');
+const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/configExpress');
+
+const app = express();
+const port = 5000;
 
 
-const app=express();
-const port=5000
+configHandlebars(app);
+configExpress(app);
 
-app.engine('hbs', handlebars.engine({
-    extname:'hbs'
-}))
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'views'))
-app.use(express.static( path.join(__dirname,'public')));
+
 app.use(routes);
 
-
-
-
-
-
-app.listen(port,()=>console.log('Server is listening on port 5000'));
+app.listen(port, () => console.log('Server is listening on port 5000'));
