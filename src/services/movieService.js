acaa0b739d7185877c4843b549e84e7191f08738
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-01-20 12:16:58 +0200

    Add details page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,7 +2,7 @@ const movies = [{
     _id: '1',
     title: 'Jungle Cruise',
     genre: 'Adventure',
-    director: 'Spilberg',
+    director: 'Spielberg',
     date: '2019',
     imageUrl: '/img/jungle-cruise.jpeg',
     rating: '5',
@@ -11,7 +11,10 @@ const movies = [{
 exports.getAll = () => {
     return movies.slice()
 }
-
+exports.getOne = (movieId) => {
+    const movie = movies.find(movie => movie._id == movieId);
+    return movie;
+}
 
 exports.create = (movieData) => {
     movieData._id = movies[movies.length - 1]._id + 1
