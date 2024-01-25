f3d2547712053f7b9b3296c888ccc09abd10e229
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-01-25 13:12:42 +0200

    create search page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const mongoose=require('mongoose')
 const configHandlebars = require('./config/configHandlebars');
 const configExpress = require('./config/configExpress');
 const routes = require('./routes');
+const { log } = require('console');
 
 const app = express();
 const port = 5000;
@@ -12,4 +13,14 @@ configExpress(app);
 
 app.use(routes);
 
+try {
+   mongoose.connect('mongodb://localhost:27017')
+   .then (()=>console.log('DB Connencted'));
+    
+} catch (error) {
+    console.log(error.message);
+}
+
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+
+
---
+++ src/models/Movie.js
@@ -0,0 +1,37 @@
+const {mongoose } = require("mongoose")
+
+const movieSchema=new mongoose.Schema({
+    title: {
+        type:String,
+        required:true
+    },
+    genre:{
+        
+    },
+    director:{
+       
+    },
+    year:{
+        
+    },
+    rating:{
+        type:Number,
+        required:true,
+        min:1,
+        max:5
+    },
+    description:{
+         type:String,
+        required:true,
+        maxLength:1000,
+    },
+imageUrl:{
+    type:String,
+        required:true,
+        match:/^http:\/\//
+}
+});
+
+const Movie=mongoose.model('Movie',movieSchema);
+
+module.exports=Movie;
