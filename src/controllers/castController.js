96ed49de71b12b34768bbcbe25d5b05499ca08a5
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-01-26 15:24:12 +0200

    create cast form

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -3,7 +3,10 @@ const router=require('express').Router();
 router.get('/create', (req,res)=>{
     res.render('cast/create')
 })
-
+router.post('/create',(req,res)=>{
+    const castData=req.body;
+    res.redirect('/')
+})
 module.exports=router;
 
 
