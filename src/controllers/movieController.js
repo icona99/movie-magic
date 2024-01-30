8089a73b91a9d2960670bea44f1c9161785d48b2
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-01-30 13:13:36 +0200

    Show movie data on attach page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,32 +6,35 @@ router.get('/create', (req, res) => {
     res.render('create');
 });
 
-router.post('/create',async (req, res) => {
+router.post('/create', async (req, res) => {
     const newMovie = req.body;
-try {
-    await  movieService.create(newMovie);
+    try {
+        await movieService.create(newMovie);
 
-    res.redirect('/');
-} catch (error) {
-    console.log(error.message);
-    res.redirect("/create");
-}
-   
+        res.redirect('/');
+    } catch (error) {
+        console.log(error.message);
+        res.redirect("/create");
+    }
+
 });
 
 router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId)
-    
+
     // TODO: This is not perfect, use handlebars helpers
     movie.rating = new Array(Number(movie.rating)).fill(true);
-    
+
     res.render('details', { movie });
 });
 
-router.get('/movies/movieId/attach',(req,res)=>{
+router.get('/movies/movieId/attach', async (req, res) => {
 
-    res.render('attach')
+    const movie = await movieService.getOne(req.params._id).lean();
+
+
+    res.render('movie/attach', { ...movie })
 })
 
 module.exports = router;
